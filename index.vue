5afc0411c940108d26f95e70edec659728415059
Wilson <wilson.lemos@bukwild.com>
2022-10-19 11:59:53 -0700

    add recaptcha support

--- index.vue
+++ index.vue
@@ -1,10 +1,11 @@
 <!-- A wrapper for forms that submit to Netlify that adds required fields -->
-
 <template lang='pug'>
 
 form.netlify-form(
 	data-netlify
 	:name='name'
+	:class="classes"
+	:data-netlify-recaptcha='recaptcha'
 	@submit.prevent='onSubmit')
 
 	//- The form fields, scoped with variables from here
@@ -20,43 +21,46 @@ form.netlify-form(
 
 <script lang='coffee'>
 export default
-
 	props:
-
 		# The form name
 		name:
 			type: String
 			required: true
-
 		# The form data
 		form:
 			type: Object
 			required: true
-
 		# The endpoint to post to
 		endpoint:
 			type: String
 			default: -> process.env.NETLIFY_FORMS_ENDPOINT || '/'
+		# Support recaptcha
+		recaptcha:
+			type: Boolean
+			default: false
 
 	data: ->
 		submitting: false
 		submitted: false
+		recaptchaError: false
+
+	computed:
+		# Should fields be readonly
+		readonly: -> @submitting or @submitted
+		# Combine form data with Netlify-specific fields
+		formData: -> { ...@form, 'form-name': @name }
+		# The computed recaptcha field value
+		classes: -> [
+			if @recaptchaError then "recaptcha-error"
+			if @submitted then "submitted"
+		]
 
 	# Check for valid input config when dev-ing
 	mounted: ->
 		if process.env.NODE_ENV != 'production'
 		then setTimeout @validateInputs, 1000 # Wait for children to mount
 
-	computed:
-
-		# Should fields be readonly
-		readonly: -> @submitting or @submitted
-
-		# Combine form data with Netlify-specific fields
-		formData: -> { ...@form, 'form-name': @name }
-
 	methods:
-
 		# Check for named inputs for each form field like Netlify expects
 		validateInputs: ->
 			for key, val of @form
@@ -67,17 +71,37 @@ export default
 		onSubmit: ->
 			return if @readonly
 			@submitting = true
-			try await @postToEndpoint()
-			catch e then return @submitting = false
-			@submitting = false
-			@submitted = true
+
+	 		# Fetch recaptcha token
+			if @recaptcha
+				try
+					token = await @$recaptcha?.getResponse()
+					@recaptchaError = false
+				catch e
+					# set recaptcha error and abort
+					@submitting = false
+					@recaptchaError = true
+					@$emit 'error', "Recaptcha"
+					return
+
+	 		# Send form data to netlify
+			try
+				await @postToEndpoint(token)
+				@submitting = false
+				@submitted = true
+				@$emit 'error', false
+			catch e
+				@$emit 'error', true
+				@submitting = false
 
 		# Do the actual post
-		postToEndpoint: -> @$axios
-			method: 'post'
-			url: @endpoint
-			data: new URLSearchParams(@formData).toString()
-			headers: 'content-type': 'application/x-www-form-urlencoded'
+		postToEndpoint: (recaptchaToken) ->
+			data = new URLSearchParams({ ...@formData, "g-recaptcha-response": recaptchaToken }).toString()
+			@$axios
+				method: 'post'
+				url: @endpoint
+				data: new URLSearchParams({ ...@formData, "g-recaptcha-response": recaptchaToken }).toString()
+				headers: 'content-type': 'application/x-www-form-urlencoded'
 
 		# Reset form data back to initial state
 		reset: ->
